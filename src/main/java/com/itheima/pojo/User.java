ff1899dec89b50fb5ad2b72d441a0354a4384032
liguangjustyou <862464147@qq.com>
2020-10-26 14:17:31 +0800

    修改了user类

--- src/main/java/com/itheima/pojo/User.java
+++ src/main/java/com/itheima/pojo/User.java
@@ -4,38 +4,6 @@ public class User {
     private Integer age;
     private String name;
     private String address;
+    private String sex;
 
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "age=" + age +
-                ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                '}';
-    }
 }
